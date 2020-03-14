9b299d3535440d89bdc3d67ef19354f5c3e968bd
Gabriel Rocha <knuckles4x@gmail.com>
2020-03-13 23:37:13 -0300

    feat: add google images search

--- index.js
+++ index.js
@@ -2,14 +2,16 @@ const robots = {
     input: require('./robots/input.js'),
     text: require('./robots/text.js'),
     state: require('./robots/state.js'),
+    image: require('./robots/image.js'),
 }
 
 async function start() {
     robots.input();
     await robots.text();
+    await robots.image();
 
     const content = robots.state.load();
-    console.dir(content, {depht: null} );
+    console.dir(content, {depht: null});
 }
 
 start();
